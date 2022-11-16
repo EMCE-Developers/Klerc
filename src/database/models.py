e70361f2abfd2015d506c93ae8893073be3cac98
Celestine <macstine@gmail.com>
2022-11-16 23:43:08 +0100

    Updated the Model
    Added username and password to User table, imported flask_login to handle user registration and login

--- src/database/models.py
+++ src/database/models.py
@@ -1,9 +1,10 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 # used th get the absolute path of this directory
 basedir = os.path.abspath(os.path.dirname(__file__))
-database_path = 'sqlite:///' + os.path.join(basedir, 'database.db')
+database_path = f"sqlite:///{os.path.join(basedir, 'database.db')}"
 
 db = SQLAlchemy()
 
@@ -19,19 +20,32 @@ def db_drop_and_create_all():
     db.create_all()
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
     email = db.Column(db.String)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
     date_created = db.Column(db.String)
-    note = db.relationship('Note', backref=db.backref('Note', lazy='true'))
-    task = db.relationship('Task', backref=db.backref('Task', lazy='true'))
+    note = db.relationship('Note', backref=db.backref('Note', lazy=True))
+    task = db.relationship('Task', backref=db.backref('Task', lazy=True))
 
     def __repre__(self):
         return f'<User {self.id} {self.first_name} {self.last_name}>'
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
 
 
 class Note(db.Model):
@@ -58,7 +72,9 @@ class Task(db.Model):
 
     # user column was removed
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    note = db.relationship('Note', backref=db.backref('note', lazy='true'))
+    note = db.relationship('Note', backref=db.backref('note', lazy=True))
 
     def __repre__(self):
         return f'<User {self.id} {self.start_time} {self.time_period}>'
+
+
