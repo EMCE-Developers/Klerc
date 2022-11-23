6455618c32a8e3f05548d42fe4be8505f4a5f4d4
Celestine <macstine@gmail.com>
2022-11-23 22:37:09 +0100

    Minor chnage

--- src/app.py
+++ src/app.py
@@ -255,7 +255,7 @@ def get_notes_by_category(category):
             "notes": result
         })
     except Exception:
-        abort(404)
+        abort(400)
 
 # Update a note by id
 @app.route('/notes/<int:note_id>', methods=['PUT', 'PATCH'])
