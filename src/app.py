35373fc287d42175ebd7ae2aea769e3b23d398be
Celestine <macstine@gmail.com>
2022-12-09 17:23:47 +0100

    Feature: get_token_auth_header
    Function to get token  from authorization header

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort, jsonify, request
+from flask import Flask, abort, jsonify, request, make_response
 from flask_cors import CORS, cross_origin
 from flask_migrate import Migrate
 # Using jwt tokens instead
@@ -7,8 +7,9 @@ from functools import wraps
 import jwt
 import uuid
 from flask_bcrypt import Bcrypt, generate_password_hash, check_password_hash
-from datetime import datetime
+from datetime import datetime, timedelta
 import logging
+from datetime import timezone
 from .database.models import Note, db, Task, Category, User, db_drop_and_create_all, setup_db
 
 
@@ -21,7 +22,7 @@ CORS(app, resources={r"*/api/*": {"origins": "*"}})
 # login_manager.init_app(app)
 migrate = Migrate(app, db)
 current_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-logging.basicConfig(filename='app.log', filemode='a', format='%(levelname)s in %(module)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logging.basicConfig(filename='app.log', filemode='w', format='%(levelname)s in %(module)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
 # with app.app_context():
 #   db_drop_and_create_all()
@@ -34,19 +35,57 @@ logging.basicConfig(filename='app.log', filemode='a', format='%(levelname)s in %
 #     except Exception:
 #         return None
 
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+def get_token_auth_header():
+    """Obtains the Access Token from the Authorization Header
+    """
+    auth = request.headers.get('Authorization', None)
+    if not auth:
+        raise AuthError({
+            'code': 'authorization_header_missing',
+            'description': 'Authorization header is expected.'
+        }, 401)
+
+    parts = auth.split()
+    if parts[0].lower() != 'bearer':
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must start with "Bearer".'
+        }, 401)
+
+    elif len(parts) == 1:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Token not found.'
+        }, 401)
+
+    elif len(parts) > 2:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must be bearer token.'
+        }, 401)
+
+    return parts[1]
+    
 def token_required(f):
     @wraps(f)
     def decorator(*args, **kwargs):
-        token = None
+        token = get_token_auth_header()
         if 'x-access-tokens' in request.headers:
-            token = request.headers['x-access-tokens']
+            #token = request.headers['x-access-tokens']
+            token = request.args.get('token')
     
         if not token:
+            print(token)
             return jsonify({'message': 'a valid token is missing'})
         try:
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
             current_user = User.query.filter_by(public_id=data['public_id']).first()
-        except Exception:
+        except:
             return jsonify({'message': 'token is invalid'})
     
         return f(current_user, *args, **kwargs)
@@ -102,9 +141,7 @@ def register():
 
         return jsonify({"success": True, "message": f"{username} created"})
     except Exception:
-        logging.exception("message")
         abort(400)
-
 
 @app.route('/login', methods=['GET', 'POST'])
 @cross_origin()
@@ -121,30 +158,23 @@ def login():
     username = body.get("username")
     password = body.get("password")
 
-    # User should be able to login with his username to access resources
-    try:
-        user = User.query.filter_by(username=username).first()
-
-        if not user and not check_password_hash(user.password, password):
-            return ({
-                "success": False,
-                "message": "Invalid username or password"
-            })
-
-        # login_user(user)
-        # return ({
-        #     "success": True,
-        #     "message": "Login successful"
-        # })
-
-        token = jwt.encode(
-            {
-                'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=5)
-            }, app.config['SECRET_KEY'], "HS256")
-        return jsonify({'token' : token})
-
-    except Exception:
-        abort(400)
+    #User should be able to login with his username to access resources
+    #try:
+    user = User.query.filter_by(username=username).first()
+    if not user and not check_password_hash(user.password, password):
+        return ({
+            "success": False,
+            "message": "Invalid username or password"
+        })
+    token = jwt.encode(
+        {
+            'public_id': user.public_id, 'exp': datetime.now(timezone.utc) + timedelta(minutes=5)
+        }, app.config['SECRET_KEY'], "HS256"
+    )
+    print(type(token))
+    return jsonify({'token' : token})
+    #except Exception:
+    #    abort(400)
 
 
 #@app.route('/logout', methods=['GET', 'POST'])
@@ -162,27 +192,27 @@ def login():
 
 
 # Made this endpoint to see what is stored in the database
-# @app.route('/users', methods=['GET'])
-# @cross_origin()
-# @login_required
-# def users():
-#     try:
-#         users = User.query.all()
-#         formatted_users = [
-#             {user.date_created: user.username, "id": user.id} for user in users]
-# 
-#         return jsonify({
-#             "success": True,
-#             "users": formatted_users
-#         })
-#     except Exception:
-#         abort(401)
+@app.route('/users', methods=['GET'])
+@cross_origin()
+def users():
+    try:
+        users = User.query.all()
+        formatted_users = [
+            {user.date_created: user.username, "id": user.public_id} for user in users]
+
+        return jsonify({
+            "success": True,
+            "users": formatted_users
+        })
+    except Exception:
+        abort(401)
 
 
 @app.route('/categories', methods=['POST'])
 @cross_origin()
 @token_required
 def new_category(current_user):
+    print(current_user.id)
     '''
     Function to add new category, expected input should come in this format, \n
     {
